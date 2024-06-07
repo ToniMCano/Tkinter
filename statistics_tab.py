2d743612d1616cef8976b558a22820375a9593f2
HomeX <homex@localhost.localdomain>
2024-06-07 08:29:39 +0200

    Cambio

--- statistics_tab.py
+++ statistics_tab.py
@@ -58,27 +58,58 @@ class StatisticsTab:
         
         self.statistics_dates = CTkFrame(self.graphics_dashboard_frame , fg_color = 'transparent' , border_width = 1 , border_color = 'gray' , width = 50)
         self.statistics_dates.grid(row = 2 , column = 0 , padx = 5 , pady = 10 , sticky = W+E) 
+        
+        self.statistics_calendar = CTkFrame(self.statistics_dates  , fg_color = 'red' , border_width = 1 , border_color = 'gray' , width = 50)
+        self.statistics_calendar.grid(row = 2 , column = 0 , columnspan = 2 , padx = 5 , pady = 10 , sticky = W+E) 
           
-        #self.statistics_dates.calendar = Calendar(self.statistics_dates , selectedmode = "day" , date_pattern = "yyyy-mm-dd" , selectbackground = 'LightBlue4') # Para poder ordenarlo en la DB "YYYY-MM-DD"
-        #self.statistics_dates.calendar.place(x = 0 , y = 0)   
-        #self.statistics_dates.calendar.bind("<<CalendarSelected>>", lambda e: StatisticsTab.test_calendar(self , e))
+        self.statistics_calendar.calendar = Calendar(self.graphics_dashboard_frame, selectedmode = "day" , date_pattern = "yyyy-mm-dd" , selectbackground = 'LightBlue4') # Para poder ordenarlo en la DB "YYYY-MM-DD"  
+        self.statistics_calendar.calendar.bind("<<CalendarSelected>>", lambda e: StatisticsTab.test_calendar(self , e))
+        self.statistics_calendar.calendar.bind("<Leave>", lambda e: StatisticsTab.forget_calendar(self , e))
+        
         
         self.calendar_from_label = CTkLabel(self.statistics_dates , text = 'Desde:' , text_color = 'Lightblue4' , fg_color = 'transparent' , bg_color = 'transparent' , height = 10)
         self.calendar_from_label.grid(row = 0 , column = 0 , padx = 5 , pady = 5, sticky = W)
 
         self.calendar_from = ttk.Entry(self.statistics_dates)
         self.calendar_from.grid(row = 1 , column = 0 , padx = 5 ,pady = 5)
-        self.calendar_from.insert(0 , datetime.now())
+        self.calendar_from.insert(0 , datetime.now().strftime("%d %B %Y"))
+        self.calendar_from.config(state = "disabled")
+        self.calendar_from.bind("<Button-1>" , lambda e: StatisticsTab.show_calendar(self , self.calendar_from , e))
+        
         self.calendar_to_label = CTkLabel(self.statistics_dates , text = 'Hasta:' , text_color = 'Lightblue4' , fg_color = 'transparent' , bg_color = 'transparent' , height = 10)
         self.calendar_to_label.grid(row = 0 , column = 1 , padx = 5 , pady = 5, sticky = W)
 
         self.calendar_to = ttk.Entry(self.statistics_dates)
         self.calendar_to.grid(row = 1 , column = 1 , padx = 5 ,pady = 5)
-        self.calendar_to.insert(0g , datetime.now())
+        self.calendar_to.insert(0 , datetime.now().strftime("%d %B %Y"))
+        self.calendar_to.config(state = "disabled")
+        self.calendar_to.bind("<Button-1>" , lambda e: StatisticsTab.show_calendar(self , self.calendar_to , e))
+        
+        
+        
+    def show_calendar(self, place , e):
+        
+        if place == "from":
+            self.calendar_from.focus_set()
+            self.statistics_calendar.calendar.place(x = 0 , y = 0)    
+            
+        else:
+            self.calendar_to.focus_set()
+            self.statistics_calendar.calendar.place(x = 0 , y = 150)  
+            
+        
+    def forget_calendar(self , e):
+
+        self.statistics_calendar.calendar.place_forget()
+        
         
     def test_calendar(self , e):
-            
-        date = self.statistics_dates.calendar.get_date() 
+        
+        date = self.statistics_calendar.calendar.get_date() 
+        
+        StatisticsTab.forget_calendar(self , e)
         
         print(date)
+        
+        
                
